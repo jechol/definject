0b5fa5bd0ae43687aa2cd00a5a7ff5ae7eedffd4
Jechol Lee <mr.jechol@gmail.com>
2020-02-29 13:45:58 +0900

    Split validate_deps checks into private functions

--- lib/definject/check.ex
+++ lib/definject/check.ex
@@ -2,27 +2,50 @@ defmodule Definject.Check do
   @moduledoc false
   @uninjectable [:erlang, Kernel, Macro, Module, Access]
 
-  def validate_deps(valid_captures, %{} = deps) do
-    valid_captures = valid_captures |> Enum.sort() |> Enum.uniq()
+  def validate_deps(used_captures, %{} = deps) do
+    used_captures = used_captures |> Enum.sort() |> Enum.uniq()
+    strict = Map.get(deps, :strict, true)
+    deps = Map.drop(deps, [:strict])
 
-    for {capture, _} <- Map.drop(deps, [:strict]) do
-      {:type, type} = Function.info(capture, :type)
-
-      if type == :local do
-        raise "Local function cannot be injected #{inspect(capture)}"
-      else
-        {:module, remote_mod} = Function.info(capture, :module)
-
-        if remote_mod in unquote(@uninjectable) do
-          raise "Uninjectable module #{inspect(remote_mod)} for #{inspect(capture)}"
-        else
-          if Map.get(deps, :strict, true) do
-            unless capture in valid_captures do
-              raise "Unused injection found #{inspect(capture)}. Add `strict: false` to disable this."
-            end
-          end
-        end
+    for {capture, _} <- deps do
+      with :ok <- confirm_type_is_external(capture),
+           :ok <- confirm_module_is_injectable(capture),
+           :ok <-
+             confirm_capture_is_used(capture, %{strict: strict, used_captures: used_captures}) do
+        :ok
       end
+    end
+  end
+
+  defp confirm_type_is_external(capture) do
+    case Function.info(capture, :type) do
+      {:type, :local} ->
+        raise "Local function cannot be injected #{inspect(capture)}"
+
+      {:type, :external} ->
+        :ok
+    end
+  end
+
+  defp confirm_module_is_injectable(capture) do
+    case Function.info(capture, :module) do
+      {:module, module} when module in @uninjectable ->
+        raise "Uninjectable module #{inspect(module)} for #{inspect(capture)}"
+
+      {:module, _} ->
+        :ok
+    end
+  end
+
+  defp confirm_capture_is_used(_, %{strict: false}) do
+    :ok
+  end
+
+  defp confirm_capture_is_used(capture, %{strict: true, used_captures: used_captures}) do
+    if capture not in used_captures do
+      raise "Unused injection found #{inspect(capture)}. Add `strict: false` to disable this."
+    else
+      :ok
     end
   end
 end
