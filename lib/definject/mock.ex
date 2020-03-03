1c39468285fa9ab90eb3f20e6fa0a14ed97d38f0
Jechol Lee <mr.jechol@gmail.com>
2020-03-03 16:38:22 +0900

    Do not reconstruct capture

--- lib/definject/mock.ex
+++ lib/definject/mock.ex
@@ -1,12 +1,6 @@
 defmodule Definject.Mock do
-  def surround_by_fn({{:&, _, [capture]}, v}) do
-    {:/, _, [mf, a]} = capture
-    {mf, _, []} = mf
-    {:., _, [m, f]} = mf
-
-    capture = function_capture_ast({m, f, a})
+  def surround_by_fn({{:&, _, [{:/, _, [_mf, a]}]} = capture, v}) do
     const_fn = {:fn, [], [{:->, [], [Macro.generate_arguments(a, __MODULE__), v]}]}
-
     {capture, const_fn}
   end
 
