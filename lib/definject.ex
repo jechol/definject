e076742ffbeab32ab81bab8e234faf1bc45a7ebb
Jechol Lee <mr.jechol@gmail.com>
2020-03-03 15:59:39 +0900

    Add :trace config

--- lib/definject.ex
+++ lib/definject.ex
@@ -49,12 +49,27 @@ defmodule Definject do
   defmacro definject(head, do: body) do
     alias Definject.Inject
 
-    if Application.get_env(:definject, :enable, Mix.env() == :test) do
-      Inject.inject_function(%{head: head, body: body, env: __CALLER__})
-    else
+    original =
       quote do
         def unquote(head), do: unquote(body)
       end
+
+    if Application.get_env(:definject, :enable, Mix.env() == :test) do
+      injected = Inject.inject_function(%{head: head, body: body, env: __CALLER__})
+
+      if Application.get_env(:definject, :trace, false) do
+        %{file: file, line: line} = __CALLER__
+
+        IO.puts("definject converting #{file}:#{line}")
+        IO.puts("Before >>>")
+        IO.puts(original |> Macro.to_string())
+        IO.puts("After >>>")
+        IO.puts(injected |> Macro.to_string())
+      end
+
+      injected
+    else
+      original
     end
   end
 
