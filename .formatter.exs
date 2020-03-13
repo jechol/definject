043daa00ea20d3b35d2816b94220b7611fc1a742
Jechol Lee <mr.jechol@gmail.com>
2020-03-14 07:22:15 +0900

    export formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,9 @@
 # Used by "mix format"
+
+locals_without_parens = [definject: 1, definject: 2]
+
 [
-  locals_without_parens: [definject: 1, definject: 2, definject: 3, let: 1],
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
