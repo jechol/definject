a4cca9192e7d73ae830d26e3b1e673084452797c
Jechol Lee <mr.jechol@gmail.com>
2020-09-02 15:01:32 +0900

    Change erlang pkg to jechol's NUR

--- shell.nix
+++ shell.nix
@@ -1,11 +1,12 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
+  jechol-nur = import sources.jechol-nur { };
   inherit (pkgs.lib) optional optionals;
-in
-pkgs.mkShell {
+in pkgs.mkShell {
   buildInputs = [
-    pkgs.beam.interpreters.erlangR23
-    pkgs.beam.packages.erlangR23.elixir_1_10
+    # pkgs.beam.interpreters.erlangR23
+    # pkgs.beam.packages.erlangR23.elixir_1_9
+    jechol-nur.beam.main.packages.erlang_23_0.elixirs.elixir_1_9_0
   ];
 }
