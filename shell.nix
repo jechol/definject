549e5685505421a8cc00a63e086406de586c9574
Jechol Lee <mr.jechol@gmail.com>
2020-09-26 09:59:28 +0900

    Remove nix/

--- shell.nix
+++ shell.nix
@@ -1,9 +1,15 @@
 let
-  sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs { };
-  jechol-nur = import sources.jechol-nur { };
-  inherit (pkgs.lib) optional optionals;
-in pkgs.mkShell {
-  buildInputs =
-    [ jechol-nur.beam.main.packages.erlang_22_0.elixirs.elixir_1_10_0 ];
+  nixpkgs = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/20.09-alpha.tar.gz";
+    sha256 = "0dxrfr0w5ksvpjwz0d2hy7x7dirnc2xk9nw1np3wr6kvdlzhs3ik";
+  }) { };
+  jechol = import (fetchTarball {
+    url = "https://github.com/jechol/nur-packages/archive/v2.1.tar.gz";
+    sha256 = "17svxz3ycgniql54cf1gry8ias6cng3krzibb93nlk0b71rh3vki";
+  }) { };
+in nixpkgs.mkShell {
+  buildInputs = [
+    jechol.beam.main.erlangs.erlang_23_0
+    jechol.beam.main.packages.erlang_23_0.elixirs.elixir_1_10_0
+  ];
 }
