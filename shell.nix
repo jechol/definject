9319d5077cb6c9e3a52d4526830a192a2a39ec17
Jechol Lee <mr.jechol@gmail.com>
2020-10-08 12:48:44 +0900

    Update dev elixir to 1.11

--- shell.nix
+++ shell.nix
@@ -4,12 +4,12 @@ let
     sha256 = "0dxrfr0w5ksvpjwz0d2hy7x7dirnc2xk9nw1np3wr6kvdlzhs3ik";
   }) { };
   jechol = import (fetchTarball {
-    url = "https://github.com/jechol/nur-packages/archive/v2.1.tar.gz";
-    sha256 = "17svxz3ycgniql54cf1gry8ias6cng3krzibb93nlk0b71rh3vki";
+    url = "https://github.com/jechol/nur-packages/archive/v2.5.tar.gz";
+    sha256 = "08dgk1ha5spxwba4275hs22298z9z3l087cw1cdllhsp8r9h4vnh";
   }) { };
 in nixpkgs.mkShell {
   buildInputs = [
-    jechol.beam.main.erlangs.erlang_23_0
-    jechol.beam.main.packages.erlang_23_0.elixirs.elixir_1_10_4
+    jechol.beam.main.erlangs.erlang_23_1
+    jechol.beam.main.packages.erlang_23_1.elixirs.elixir_1_11_0
   ];
 }
